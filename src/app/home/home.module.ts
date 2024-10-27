77d18a0b39189dd4c5a4236328596b0a4e234bc0
Ilija <ilija0125@gmail.com>
2024-10-27 18:08:30 +0100

    init login/regitser layout

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeLayoutComponent } from './components/home-layout/home-layout.component';
 import { HomeSeachComponent } from './components/home-seach/home-seach.component';
+import { HomeFeaturedApartmentsComponent } from './components/home-featured-apartments/home-featured-apartments.component';
+import { PopularDestinationsComponent } from './components/popular-destinations/popular-destinations.component';
+import { HomeFlightsComponent } from './components/home-flights/home-flights.component';
 
 
 @NgModule({
   declarations: [
     HomeLayoutComponent,
-    HomeSeachComponent
+    HomeSeachComponent,
+    HomeFeaturedApartmentsComponent,
+    PopularDestinationsComponent,
+    HomeFlightsComponent
   ],
   imports: [
     CommonModule,
