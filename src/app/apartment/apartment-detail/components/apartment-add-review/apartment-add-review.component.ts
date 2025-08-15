2eeb348dab0f0120035058773d54d143a76b5f91
Ilija <ilija0125@gmail.com>
2025-08-15 16:57:32 +0200

    create rating

--- src/app/apartment/apartment-detail/components/apartment-add-review/apartment-add-review.component.ts
+++ src/app/apartment/apartment-detail/components/apartment-add-review/apartment-add-review.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { ReviewCategory } from './enums/review-category';
+import { Component, Input } from '@angular/core';
+import { RATING_CATEGORY } from './enums/review-category';
 import { BlApartmentReviewFormService } from './services/forms/bl-apartment-review-form.service';
 import { Spinner } from '../../../../core/functions/spinner';
 import { IApartmentReview, IApartmentReviewRequest } from './interfaces/i-apartment-review';
+import { FormArray } from '@angular/forms';
 
 @Component({
     selector: 'app-apartment-add-review',
@@ -17,33 +18,53 @@ export class ApartmentAddReviewComponent {
   ) { }
 
   form = this.formService.getForm();
+  @Input('apartmentId') apartmentId: number;
 
-  categories = ReviewCategory;
+  categories = RATING_CATEGORY;
   stars: number[] = [1, 2, 3, 4, 5];
 
   ratings = {
-    [ReviewCategory.Price]: 0,
-    [ReviewCategory.Service]: 0,
-    [ReviewCategory.Safety]: 0,
-    [ReviewCategory.Entertainment]: 0,
-    [ReviewCategory.Accessibility]: 0,
-    [ReviewCategory.Support]: 0
+    [RATING_CATEGORY.Price]: 0,
+    [RATING_CATEGORY.Service]: 0,
+    [RATING_CATEGORY.Safety]: 0,
+    [RATING_CATEGORY.Entertainment]: 0,
+    [RATING_CATEGORY.Accessibility]: 0,
+    [RATING_CATEGORY.Support]: 0
   };
 
-  getStarSrc(category: ReviewCategory, starIndex: number): string {
+  getStarSrc(category: RATING_CATEGORY, starIndex: number): string {
     return starIndex <= this.ratings[category]
       ? 'assets/images/page/tour-detail/star-big.svg'
       : 'assets/images/page/tour-detail/star-big-transparent.svg';
   }
 
-  updateRating(category: ReviewCategory, starIndex: number): void {
+  updateRating(category: RATING_CATEGORY, starIndex: number): void {
     this.ratings[category] = starIndex;
-    this.form.get(category.toLowerCase()).setValue(starIndex);
+    let categoryId = this.getCategoryId(category);
+    let formArray = this.form.get('values') as FormArray;
+    let valueIndex = formArray.controls.findIndex(control => control.get('id').value === categoryId);
+
+    if (valueIndex !== -1) {
+      formArray.at(valueIndex).get('value')?.setValue(starIndex);
+    }
   }
+
+  private getCategoryId(category: RATING_CATEGORY): number {
+  switch (category) {
+    case RATING_CATEGORY.Price: return 1;
+    case RATING_CATEGORY.Service: return 2;
+    case RATING_CATEGORY.Safety: return 3;
+    case RATING_CATEGORY.Entertainment: return 4;
+    case RATING_CATEGORY.Accessibility: return 5;
+    case RATING_CATEGORY.Support: return 6;
+    default: return 0;
+  }
+}
 
 
   sumbit(): void {
     Spinner.show();
+    this.form.get('apartmentId').setValue(this.apartmentId);
     this.formService.submit().subscribe({
       next: (data) => {
         Spinner.hide()
